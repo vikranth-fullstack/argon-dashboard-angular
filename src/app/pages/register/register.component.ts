107f6f10a07fd8aa90249298da6dbfeb64cb7737
Vikranth Reddy <vikranthr@goextramile.in>
2024-07-25 02:59:20 +0530

    updated code

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -29,12 +29,50 @@ export class RegisterComponent implements OnInit {
   percentage = 0;
 
   days: string[] = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+  selectedFile: File | null = null; // Property to store the selected file
+  gstFile: File | null = null;
 
+  formatBytes(bytes: number): string {
+    if (bytes === 0) return '0 Bytes';
 
+    const k = 1024;
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
 
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + ' ' + sizes[i];
+  }
+  onGstFileSelected(event: any): void {
+    const file: File = event.target.files[0];
+    this.gstFile = file;
+    // You can also reset the form control if needed:
+    this.businessForm.get('gstFileName').setValue(file.name);
+  }
+
+  removeGstFile(): void {
+    this.gstFile = null;
+    this.businessForm.get('gstFileName').setValue('');
+    this.businessForm.get('gstFile').setValue(null);
+    const fileInput = document.getElementById('uploadGstFile') as HTMLInputElement;
+    fileInput.value = null;
+    // You can also reset the form control:
+    // this.businessForm.get('gstNumber').setValue('');
+  }
+  downloadGstFile(): void {
+    if (this.gstFile) {
+      const url = window.URL.createObjectURL(this.gstFile);
+      const a = document.createElement('a');
+      document.body.appendChild(a);
+      a.style.display = 'none';
+      a.href = url;
+      a.download = this.gstFile.name;
+      a.click();
+      window.URL.revokeObjectURL(url);
+    }
+  }
   constructor(private fb: FormBuilder, private firebaseService: FirebaseService) { }
   async onFileSelected(event: any) {
     const file: File = event.target.files[0];
+    this.selectedFile = file; 
     if (file) {
       const reader = new FileReader();
       reader.onload = (e: any) => {
@@ -83,7 +121,10 @@ export class RegisterComponent implements OnInit {
       mobileNumber: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(10)]],
       userName: ['', [Validators.required]],
       // Accept Terms checkbox
-      acceptTerms: [false, Validators.requiredTrue]
+      acceptTerms: [false, Validators.requiredTrue],
+      gstNumber: [''], // GST number text input,
+      gstFileName:[''],
+      gstFile: [null] // GST file upload control
     });
 
     this.days.forEach(day => {
@@ -100,7 +141,11 @@ export class RegisterComponent implements OnInit {
     })
     this.fetchBusinessCategories();
     this.initForm();
-    this.businessForm.patchValue({ businessCategory: '2V27pgnQrigqObYyrzSo' });
+    this.businessForm.patchValue({ businessCategory: '2V27pgnQrigqObYyrzSo','tuesdayOpen': true,tuesdayStartTime: "15:39"});
+
+
+    //tuesdayOpen: true
+//tuesdayStartTime: "15:15"
   }
   onMobileNumberInput(event: any): void {
     const input = event.target as HTMLInputElement;
@@ -144,6 +189,24 @@ export class RegisterComponent implements OnInit {
     });
   }
   submitForm() {
+
+
+     //File Upload
+     this.currentFileUpload = new FileUpload(this.gstFile);
+
+
+     this.firebaseService.pushFileToStorage(this.currentFileUpload, "/business_registration_gst_files").subscribe({
+       next: downloadURL => {
+         debugger;
+         console.log('File uploaded successfully. Download URL:', downloadURL);
+         // Handle further operations (e.g., save download URL to database)
+       },
+       error: error => {
+         console.error('Error uploading file:', error);
+         // Handle error gracefully
+       }
+     });
+
     if (this.businessForm.valid) {
       console.log(this.businessForm.value); // Replace with your form submission logic
     } else {
